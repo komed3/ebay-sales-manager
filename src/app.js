8d5d2cdcf1640b23918f85a9cc63738ffbae1cb6
komed3 <pk97@outlook.de>
2025-10-25 19:08:24 +0200

    server config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
-import express from 'express';
+import { join } from 'node:path';
+import express, { static as serveStatic } from 'express';
 
+const cwd = process.cwd();
 const app = express();
 
+// Set up view engine
+app.set( 'views', join( cwd, 'views' ) );
+app.set( 'view engine', 'pug' );
+
+// Serve static files
+app.use( '/fonts', serveStatic( join( cwd, 'public/fonts' ) ) );
+app.use( '/images', serveStatic( join( cwd, 'public/images' ) ) );
+app.use( '/css', serveStatic( join( cwd, 'public/css' ) ) );
+app.use( '/js', serveStatic( join( cwd, 'public/js' ) ) );
+
+// Listen on port 3000
 app.listen( 3000 );
