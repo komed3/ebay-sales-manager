508561c31302addc5978cad3d29e846d13ef9474
komed3 <pk97@outlook.de>
2025-10-28 11:32:06 +0100

    add map view

--- src/controller/map.js
+++ src/controller/map.js
@@ -1,1 +1,14 @@
-export function map ( req, res, next ) { res.render( 'map', { path: '/map', title: 'Karte' } ) }
+import { getOrders } from '../storage.js';
+
+export function map ( _, res ) {
+
+    const locations = getOrders().map( o => o.location ? {
+        ...o.location, ...{ type: o.orderType, uuid: o.__uuid }
+    } : null ).filter( Boolean );
+
+    res.render( 'map', {
+        path: '/map', title: 'Karte',
+        data: locations
+    } );
+
+}
