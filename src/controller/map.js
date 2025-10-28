755099495a628cc46ce5ea7d7a54d5cf86fdea25
komed3 <pk97@outlook.de>
2025-10-28 17:40:02 +0100

    add additional map data

--- src/controller/map.js
+++ src/controller/map.js
@@ -3,7 +3,12 @@ import { getOrders } from '../storage.js';
 export function map ( _, res ) {
 
     const locations = getOrders().map( o => o.location ? {
-        ...o.location, ...{ type: o.orderType, uuid: o.__uuid }
+        ...o.location, ...{
+            type: o.orderType, uuid: o.__uuid,
+            number: o.orderNumber,
+            name: o.customer.name,
+            address: o.customer.address
+        }
     } : null ).filter( Boolean );
 
     res.render( 'map', {
