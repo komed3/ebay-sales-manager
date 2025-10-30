2fa570d3215a2e41bcaf27f8cff75e7da561bee9
komed3 <pk97@outlook.de>
2025-10-30 19:06:08 +0100

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,17 @@
+import { cwd } from './config.js';
+import i18next from 'i18next';
+import FsBackend from 'i18next-fs-backend';
+import { handle } from 'i18next-http-middleware';
+
+await i18next.use( FsBackend ).init( {
+    fallbackLng: 'en-US',
+    preload: [ 'en-US', 'de-DE' ],
+    backend: {
+        loadPath: join( cwd, 'locales/{{lng}}.json' )
+    },
+    interpolation: {
+        escapeValue: false
+    }
+} );
+
+export const i18nHandler = handle( i18next );
