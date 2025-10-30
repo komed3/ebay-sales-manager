0bbb944d6398c4b9c68bd6301edb61cf5208594b
komed3 <pk97@outlook.de>
2025-10-30 09:55:57 +0100

    rework auth to work with new user system

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,5 +1,5 @@
-import { config } from '../config.js';
 import bcrypt from 'bcrypt';
+import { getUser } from '../storage.js';
 
 export function login ( _, res ) { res.render( 'login' ) }
 
@@ -15,23 +15,27 @@ export async function auth ( req, res ) {
         error: 'Bitte Benutzername und Passwort angeben.'
     } );
 
-    // Get the password hash
-    const envKey = `PASSWORD__${ username.toUpperCase() }`;
-    const storedHash = config[ envKey ];
+    // Get user from storage
+    const user = getUser( username );
 
-    if ( ! storedHash ) return res.render( 'login', {
+    if ( ! user ) return res.render( 'login', {
         error: 'Unbekannter Benutzer.'
     } );
 
     // Check the password
-    const valid = await bcrypt.compare( password, storedHash );
+    const valid = await bcrypt.compare( password, user.pass );
 
     if ( ! valid ) return res.render( 'login', {
         error: 'Falsches Passwort.'
     } );
 
     // Success
-    req.session.user = { name: username };
+    req.session.user = {
+        name: username,
+        mail: user.mail,
+        settings: user.settings || {}
+    };
+
     res.redirect( '/' );
 
 }
