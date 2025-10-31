b0ece89ca3b7e449c38cd9a0e59e6e9e2afed48a
komed3 <pk97@outlook.de>
2025-10-31 12:44:06 +0100

    Update install.js

--- scripts/install.js
+++ scripts/install.js
@@ -3,29 +3,30 @@ import { join } from 'node:path';
 
 const cwd = process.cwd();
 
-// Create directories
-mkdirSync( join( cwd, 'data/reports' ), { recursive: true } );
-mkdirSync( join( cwd, 'data/upload/tmp' ), { recursive: true } );
-
-// Create files
+// Helper to create file if it not exists
 const createFile = ( path, payload ) => {
 
     const fullPath = join( cwd, 'data', path );
 
     if( ! existsSync( fullPath ) ) writeFileSync(
-        fullPath,
-        JSON.stringify( payload, null, 2 ),
-        'utf8'
+        fullPath, JSON.stringify( payload, null, 2 ), 'utf8'
     );
 
 }
 
-createFile( 'customers.json', {} );
-createFile( 'orders.json', [] );
+// Create directories
+mkdirSync( join( cwd, 'data/reports' ), { recursive: true } );
+mkdirSync( join( cwd, 'data/upload/tmp' ), { recursive: true } );
+
+// Create files
 createFile( 'annualReports.json', {} );
-createFile( 'monthlyReports.json', {} );
-createFile( 'users.json', {} );
 createFile( 'calendar.json', [] );
+createFile( 'customers.json', {} );
+createFile( 'monthlyReports.json', {} );
+createFile( 'orders.json', [] );
+createFile( 'users.json', {} );
+
+// Populate stats
 createFile( 'stats.json', {
     orderCount: 0, customerCount: 0, shippingCount: 0, pickupCount: 0, totalRevenue: 0,
     shippingRevenue: 0, pickupRevenue: 0, totalShipping: 0, totalFees: 0, totalRefund: 0,
