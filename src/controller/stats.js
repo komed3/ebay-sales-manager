710f7cc44b40352bfdfdefacd2afbe555e85b1bf
komed3 <pk97@outlook.de>
2025-10-31 10:01:41 +0100

    i18n support for stats page

--- src/controller/stats.js
+++ src/controller/stats.js
@@ -5,7 +5,7 @@ export function stats ( req, res ) {
     const query = { ...{ type: 'monthly' }, ...req.query };
 
     res.render( 'stats', {
-        path: '/stats', title: 'Statistik',
+        path: '/stats', title: req.t( 'stats._meta.title' ),
         query: query,
         reports: query.type === 'annual' ? getAnnualReports() : getMonthlyReports(),
         stats: getOrderStats(),
